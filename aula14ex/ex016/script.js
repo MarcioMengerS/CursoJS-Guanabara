744fd964c8644d8338240b6d9fc2256d05878187
Marcio Menger de Souza <gm717gem@gmail.com>
2020-05-18 19:13:04 -0300

    cont. exercícios

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,15 +1,30 @@
 //alert('Olá!')
 
 function contar(){
+    //valores obtidos da página HTML modelo são colocados em variáveis
     let ini = window.document.getElementById('in')
     let fi = window.document.getElementById('fi')
     let pas = window.document.getElementById('pas')
     let conta = window.document.querySelector('div#conta')
 
-    // se não transformar em numero dá esse erro[object HTMLInputElement]
-    i = Number(ini.value)
+    //Transforme as váriaveis obtidas em números, caso contrário aparecerá a mensagem de erro:[object HTMLInputElement]
+    ini = Number(ini.value)
+    pas = Number(pas.value)
+    fi = Number(fi.value)
 
-    //devolve resultado na página HTML na div conta
-    conta.innerHTML = `testando impressáo n tela do valor inicio: ${i}`
+    //validação
+    if(ini>fi){
+        window.alert('Valor de Início tem que ser menor que o de FIM!!')
+    }
+
+    //A estrutura de repetição serve para imprimir e somar os números de passo e início. Enquanto fim for maior ou igual ao inicio.
+    while(fi>=ini){
+        //Sem o += o valor a ser impresso na página será o último do loop
+        //devolve resultado na página HTML modelo na div de id=conta
+        conta.innerHTML += `${ini} `
+        ini = pas+ini
+    }
+    
+    //conta.innerHTML = `testando impressão na tela valor inicio: ${pas}`
 
 }
