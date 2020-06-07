f6cc559b9f8daef354ac925d4b36c13f2f42909e
Marcio Menger de Souza <gm717gem@gmail.com>
2020-06-07 18:30:12 -0300

    correção exercicio tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,28 @@
-alert('Olá!')
+function tabuada(){
+    //valores obtidos da página HTML da tabuada são colocados em variáveis.
+    let num = window.document.getElementById("txtn")
+    let tab = window.document.getElementById('seltab')
+    if(num.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    }else{
+        //transforma o texto da variável "num" em número
+        let n = Number(num.value)
+        //limpa a tela/tabuada anterior
+        tab.innerHTML = ''
+        //o laço faz a multiplicação e mostra na tela
+        let c = 1
+        while(c <= 10){
+            //cria varias tags <option> no html de forma dinâmica e coloca dentro da váriavel "item"
+            let item = document.createElement('option')
+            //coloca o cálculo que é um texto dentro do <option> através da variável "item"
+            item.text = `${n}  X ${c} = ${n*c}`
+            //serve para saber qual linha foi selecionada
+            item.value = `tab${c}`
+            //adiciona um elemento filho que é um "item"
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+    
+}
--- aula14ex/modelo/script.js
+++ aula14ex/modelo/script.js
@@ -1,15 +1,1 @@
-function gerar(){
-    //valores obtidos da página HTML da tabuada são colocados em variável
-    let numero = window.document.getElementById("num")
-    let resultado = window.document.querySelector('div#result')
-
-    let cont= 0
-    numero = Number(numero.value)
-
-    while(cont<=10){
-        numero*cont
-        resultado.innerHTML += ` ${numero}X${cont} = ${numero*cont}<br>`
-
-        cont++
-    }
-}
+alert('Olá!')
