75ace1280df17c598566fe012bd65defda8acd65
Marcio Menger de Souza <gm717gem@gmail.com>
2020-06-07 17:22:27 -0300

    Exercício após correção

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,29 +1,37 @@
 
 function contar(){
     //valores obtidos da página HTML modelo são colocados em variáveis
-    let ini = window.document.getElementById('in')
-    let fi = window.document.getElementById('fi')
-    let pas = window.document.getElementById('pas')
-    let conta = window.document.querySelector('div#conta')
-
-    //Transforme as váriaveis obtidas em números, caso contrário aparecerá a mensagem de erro:[object HTMLInputElement]
-    ini = Number(ini.value)
-    pas = Number(pas.value)
-    fi = Number(fi.value)
+    let ini = window.document.getElementById('txti')
+    let fim = window.document.getElementById('txtf')
+    let passo = window.document.getElementById('txtp')
+    let res = window.document.getElementById('res')
 
     //validação
-    if(ini>fi){
-        window.alert('Valor de Início tem que ser menor que o de FIM!!')
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        //window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossivel contar!'
+    }else{
+        res.innerHTML = 'Contando: <br>'
+        //Transforme as váriaveis obtidas em números, caso contrário aparecerá a mensagem de erro:[object HTMLInputElement]
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0){
+            window.alert('Passo Inválido! Considerando passo 1')
+            p = 1
+        }
+        //A estrutura de repetição serve para imprimir e somar os números de passo e início. Enquanto fim for maior ou igual ao inicio.
+        if(i < f){
+            //contagem crescente
+            for(let c = i; c <= f; c += p){
+            res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }else{
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p){
+            res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
-
-    //A estrutura de repetição serve para imprimir e somar os números de passo e início. Enquanto fim for maior ou igual ao inicio.
-    while(fi>=ini){
-        //Sem o += o valor a ser impresso na página será o último do loop
-        //devolve resultado na página HTML modelo na div de id=conta
-        conta.innerHTML += `${ini} `
-        ini = pas+ini
-    }
-    
-    //conta.innerHTML = `testando impressão na tela valor inicio: ${pas}`
-
 }
